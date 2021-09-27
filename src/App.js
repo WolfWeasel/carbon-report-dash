5d717f1fab682a48ae8b07d5ac9c677584a4c7b7
Mark Syrzysko <markio9999@gmail.com>
2021-09-27 08:36:59 +0200

    reconfigured the react style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-// import { useState } from "react";
 import Graph from "./components/Graph";
 import Table from "./components/Table/Table";
 import carbonData from "./data/MockEmissionsData.json";
@@ -20,30 +19,18 @@ class App extends React.Component {
     }
 
     this.generateReport = this.generateReport.bind(this);
-    // this.stringToDate = this.stringToDate.bind(this);
   }
 
   generateReport() {
     this.setState({
       carbonData: carbonData
     });
-    // console.log(carbonData);
-    // console.log(this.state.carbonDataRef);
 
-    // this.setState({carbonDataRef: carbonData});
-
-    
     this.setState({carbonDataRef: carbonData.filter((item) => 
     this.stringToDate(item.date).getTime() >= this.state.fromDate.getTime() && this.stringToDate(item.date) <= this.state.toDate.getTime()
       )});
 
       console.log(this.state.carbonDataRef);
-    // if(this.state.carbonData.length === 0) {
-    //   isData = false;
-    // } else {
-    //   isData = true;
-    // }
-    // console.log(isData);
   }
   stringToDate(dateStr) {
     return new Date(dateStr);
@@ -89,61 +76,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// function App() {
-//   const [fromDate, setFromDate] = useState(new Date('2021-07-01'));
-//   const [toDate, setToDate] = useState(new Date('2021-07-04'));
-//   const [carbonDataJ, setCarbonDataJ] = useState(carbonData)
-//   console.log(carbonDataJ);
-//   let isData = true;
-
-//   function stringToDate(dateStr) {
-//     return new Date(dateStr);
-//   }
-//   function generateReport() {
-    
-//     setCarbonDataJ(carbonData.filter((item) => 
-//     stringToDate(item.date).getTime() >= fromDate.getTime() && stringToDate(item.date) <= toDate.getTime()
-//       ));
-//     if(carbonDataJ.length == 0) {
-//       isData = false;
-//     } else {
-//       isData = true;
-//     }
-//     console.log(isData);
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Carbon Report - Dashboard</h1>
-
-//       <div className="dateSelect">
-//             <div className="mar-left">From </div>
-//             <DatePicker className="input"
-//               selected={fromDate}
-//               onChange={(date) => setFromDate(date)}
-//             />
-//             <div className="mar-left">To </div>
-//             <DatePicker className="input"
-//               selected={toDate}
-//               onChange={(date) => setToDate(date)}
-//             />
-//             <button className="btn" onClick={generateReport}>Generate</button>
-//           </div>
-
-//           <div className="graphBox">
-//             <Graph data={carbonData} />
-//           </div>
-
-//           <div className="tableBox">
-//             <Table data={carbonData} />
-//           </div>
-
-//           <div className="emissionIndication">
-//             <CarbonStatus data={carbonData} />
-//           </div>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,87 +1,177 @@
-import { useState } from "react";
 import { Bar } from "react-chartjs-2";
+import React from "react";
 
+class Graph extends React.Component {
+  constructor(props) {
+    super();
 
-const drawLine = {
-  afterDraw: function (chart) {
-    if (typeof chart.config.options.lineAt != "undefined") {
-      var lineAt = chart.config.options.lineAt;
-      var ctxPlugin = chart.ctx;
-      var xAxe = chart.scales[chart.config.options.scales.xAxes.id];
-      var yAxe = chart.scales[chart.config.options.scales.yAxes.id];
+    this.state = {
+      carbonData: props.data,
+    };
+    this.setState(this.state);
+  }
 
-      if (yAxe.min !== 0) return;
+  render() {
+    const drawLine = {
+      afterDraw: function (chart) {
+        if (typeof chart.config.options.lineAt != "undefined") {
+          var lineAt = chart.config.options.lineAt;
+          var ctxPlugin = chart.ctx;
+          var xAxe = chart.scales[chart.config.options.scales.xAxes.id];
+          var yAxe = chart.scales[chart.config.options.scales.yAxes.id];
 
-      ctxPlugin.lineWidth = 2;
-      ctxPlugin.strokeStyle = "red";
-      ctxPlugin.beginPath();
-      lineAt = (lineAt - yAxe.min) * (100 / yAxe.max);
-      lineAt = ((100 - lineAt) / 100) * yAxe.height + yAxe.top;
-      ctxPlugin.moveTo(xAxe.left, lineAt);
-      ctxPlugin.lineTo(xAxe.right, lineAt);
-      ctxPlugin.stroke();
-    }
-  },
-};
+          if (yAxe.min !== 0) return;
 
-const options = {
-  lineAt: 0.25,
-  scales: {
-    xAxes: [
-      {
-        scaleLabel: {
-          display: true,
-          labelString: "Amount",
-        },
+          ctxPlugin.lineWidth = 2;
+          ctxPlugin.strokeStyle = "red";
+          ctxPlugin.beginPath();
+          lineAt = (lineAt - yAxe.min) * (100 / yAxe.max);
+          lineAt = ((100 - lineAt) / 100) * yAxe.height + yAxe.top;
+          ctxPlugin.moveTo(xAxe.left, lineAt);
+          ctxPlugin.lineTo(xAxe.right, lineAt);
+          ctxPlugin.stroke();
+        }
       },
-    ],
-    yAxes: [
-      {
-        scaleLabel: {
-          display: true,
-          labelString: "Days",
-        },
-        ticks: {
-          min: 0,
-        },
-      },
-    ],
-  },
-};
+    };
 
-function Graph(prop) {
-  const [carbonData, setdata] = useState(prop.data);
-//   const carbonData = prop.data;
-
-  let barData = {
-    labels: [...new Set(carbonData.map((data) => data.date))],
-    datasets: [
-      {
-        label: "CO",
-        backgroundColor: "rgb(81,77,203)",
-        data: [
-          ...carbonData.map((data) => {
-            if (data.gas === "CO") return parseFloat(data.amount);
-            return null;
-          }),
-        ].filter((n) => n),
-      },
-      {
-        label: "CO-2",
-        backgroundColor: "rgb(208,67,19)",
-        data: [
-          ...carbonData
-            .map((data) => {
-              if (data.gas === "CO2") return parseFloat(data.amount);
-              return null;
-            })
-            .filter((n) => n),
+    const options = {
+      lineAt: 0.25,
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Amount",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Days",
+            },
+            ticks: {
+              min: 0,
+            },
+          },
         ],
       },
-    ],
-  };
+    };
 
-  return <Bar data={barData} options={options} plugins={[drawLine]} />;
+    let barData = {
+      labels: [...new Set(this.state.carbonData.map((data) => data.date))],
+      datasets: [
+        {
+          label: "CO",
+          backgroundColor: "rgb(81,77,203)",
+          data: [
+            ...this.state.carbonData.map((data) => {
+              if (data.gas === "CO") return parseFloat(data.amount);
+              return null;
+            }),
+          ].filter((n) => n),
+        },
+        {
+          label: "CO-2",
+          backgroundColor: "rgb(208,67,19)",
+          data: [
+            ...this.state.carbonData
+              .map((data) => {
+                if (data.gas === "CO2") return parseFloat(data.amount);
+                return null;
+              })
+              .filter((n) => n),
+          ],
+        },
+      ],
+    };
+
+    return <Bar data={barData} options={options} plugins={[drawLine]} />;
+  }
 }
-
 export default Graph;
+
+// const drawLine = {
+//   afterDraw: function (chart) {
+//     if (typeof chart.config.options.lineAt != "undefined") {
+//       var lineAt = chart.config.options.lineAt;
+//       var ctxPlugin = chart.ctx;
+//       var xAxe = chart.scales[chart.config.options.scales.xAxes.id];
+//       var yAxe = chart.scales[chart.config.options.scales.yAxes.id];
+
+//       if (yAxe.min !== 0) return;
+
+//       ctxPlugin.lineWidth = 2;
+//       ctxPlugin.strokeStyle = "red";
+//       ctxPlugin.beginPath();
+//       lineAt = (lineAt - yAxe.min) * (100 / yAxe.max);
+//       lineAt = ((100 - lineAt) / 100) * yAxe.height + yAxe.top;
+//       ctxPlugin.moveTo(xAxe.left, lineAt);
+//       ctxPlugin.lineTo(xAxe.right, lineAt);
+//       ctxPlugin.stroke();
+//     }
+//   },
+// };
+
+// const options = {
+//   lineAt: 0.25,
+//   scales: {
+//     xAxes: [
+//       {
+//         scaleLabel: {
+//           display: true,
+//           labelString: "Amount",
+//         },
+//       },
+//     ],
+//     yAxes: [
+//       {
+//         scaleLabel: {
+//           display: true,
+//           labelString: "Days",
+//         },
+//         ticks: {
+//           min: 0,
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// function Graph(prop) {
+//   const [carbonData, setdata] = useState(prop.data);
+// //   const carbonData = prop.data;
+
+//   let barData = {
+//     labels: [...new Set(carbonData.map((data) => data.date))],
+//     datasets: [
+//       {
+//         label: "CO",
+//         backgroundColor: "rgb(81,77,203)",
+//         data: [
+//           ...carbonData.map((data) => {
+//             if (data.gas === "CO") return parseFloat(data.amount);
+//             return null;
+//           }),
+//         ].filter((n) => n),
+//       },
+//       {
+//         label: "CO-2",
+//         backgroundColor: "rgb(208,67,19)",
+//         data: [
+//           ...carbonData
+//             .map((data) => {
+//               if (data.gas === "CO2") return parseFloat(data.amount);
+//               return null;
+//             })
+//             .filter((n) => n),
+//         ],
+//       },
+//     ],
+//   };
+
+//   return <Bar data={barData} options={options} plugins={[drawLine]} />;
+// }
+
+// export default Graph;
